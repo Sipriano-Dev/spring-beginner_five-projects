ee772e5ba96efcfb17eef78515e26934edc41af0
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 22:03:05 -0300

    Setter based dependency injection

--- learn-spring-framework/src/main/java/com/spring/core/di/MessageSender.java
+++ learn-spring-framework/src/main/java/com/spring/core/di/MessageSender.java
@@ -12,21 +12,35 @@ public class MessageSender {
     private MessageService smsService;
 
 //    @Autowired
-    public MessageSender(@Qualifier("smsService") MessageService messageService){
-        this.messageService = messageService;
-        System.out.println("Constructor Injection");
-    }
+//    public MessageSender(@Qualifier("smsService") MessageService messageService){
+//        this.messageService = messageService;
+//        System.out.println("Constructor Injection");
+//    }
+//
+//    @Autowired
+//    public MessageSender(@Qualifier("smsService") MessageService messageService, MessageService smsService) {
+//        this.messageService = messageService;
+//        this.smsService = smsService;
+//        System.out.println("Constructor Injection 2");
+//    }
+
 
     @Autowired
-    public MessageSender(@Qualifier("smsService") MessageService messageService, MessageService smsService) {
+    public void setMessageService(@Qualifier("emailService") MessageService messageService) {
         this.messageService = messageService;
+        System.out.println("Setter based dependency injection 1");
+    }
+
+    @Autowired //You do note need to use qualify here, but you can if you want
+    public void setSmsService(@Qualifier("smsService") MessageService smsService) {
         this.smsService = smsService;
-        System.out.println("Constructor Injection 2");
+        System.out.println("Setter based dependency injection 2");
     }
 
     public void sendMessage(String message){
         this.messageService.sendMessage(message);
         this.smsService.sendMessage(message);
     }
+
 
 }
