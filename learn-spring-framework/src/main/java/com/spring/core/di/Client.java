9c81b8600e1bce3f560ad439e81199fb5259cb4e
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 22:01:01 -0300

    Example dependency injection using Spring Framework

---
+++ learn-spring-framework/src/main/java/com/spring/core/di/Client.java
@@ -0,0 +1,26 @@
+package com.spring.core.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+
+        String message = "Hi, Good morning, have a nice day!";
+
+//        EmailService emailService = new EmailService();
+//        SMSService smsService = new SMSService();
+//        MessageSender messageSender = new MessageSender(emailService);
+//
+//        messageSender.sendMessage(message);
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MessageSender messageSender = applicationContext.getBean(MessageSender.class);
+        messageSender.sendMessage(message);
+
+    }
+
+}
---
+++ learn-spring-framework/src/main/java/com/spring/core/di/MessageService.java
@@ -0,0 +1,7 @@
+package com.spring.core.di;
+
+public interface MessageService {
+
+    void sendMessage(String message);
+
+}
