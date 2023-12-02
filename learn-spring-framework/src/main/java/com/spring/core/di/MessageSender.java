553aaaffeda9a7041b05659014455f283ffd50bc
Anderson Sipriano <Andsipriano@gmail.com>
2023-12-01 22:02:01 -0300

    Constructor based dependency injection

--- learn-spring-framework/src/main/java/com/spring/core/di/MessageSender.java
+++ learn-spring-framework/src/main/java/com/spring/core/di/MessageSender.java
@@ -9,14 +9,24 @@ import org.springframework.stereotype.Component;
 public class MessageSender {
 
     private MessageService messageService;
+    private MessageService smsService;
+
+//    @Autowired
+    public MessageSender(@Qualifier("smsService") MessageService messageService){
+        this.messageService = messageService;
+        System.out.println("Constructor Injection");
+    }
 
     @Autowired
-    public MessageSender(@Qualifier("emailService") MessageService messageService){
+    public MessageSender(@Qualifier("smsService") MessageService messageService, MessageService smsService) {
         this.messageService = messageService;
+        this.smsService = smsService;
+        System.out.println("Constructor Injection 2");
     }
 
     public void sendMessage(String message){
         this.messageService.sendMessage(message);
+        this.smsService.sendMessage(message);
     }
 
 }
